06eabb528ab2e497d2321036f2b0e9fc7570d605
Chris Down <chris@chrisdown.name>
2017-02-07 12:47:49 +0000

    Support non-yaourt tools

--- yaourt.py
+++ yaourt.py
@@ -3,20 +3,28 @@
 from ansible.module_utils.basic import *
 
 
+TOOL_TO_INSTALL_CMD_MAP = {
+    'pacaur': ('pacaur', '--noconfirm', '--noedit', '-S'),
+    'yaourt': ('yaourt', '--noconfirm', '-S'),
+}
+
+
 def package_installed(module, package_name):
     cmd = ['pacman', '-Q', package_name]
     exit_code, _, _ = module.run_command(cmd, check_rc=False)
     return exit_code == 0
 
 
-def install_packages(module, package_name):
+def install_packages(module, package_name, tool):
     if package_installed(module, package_name):
         module.exit_json(
             changed=False,
             msg='package already installed',
         )
 
-    cmd = ['yaourt', '--noconfirm', '-S', package_name]
+    assert tool in TOOL_TO_INSTALL_CMD_MAP
+
+    cmd = TOOL_TO_INSTALL_CMD_MAP[tool] + [package_name]
     module.run_command(cmd, check_rc=True)
 
     module.exit_json(
@@ -56,6 +64,10 @@ def main():
                 'default': 'present',
                 'choices': ['present', 'absent'],
             },
+            'tool': {
+                'default': 'pacaur',
+                'choices': ['pacaur', 'yaourt'],
+            },
             'recurse': {
                 'default': False,
                 'type': 'bool',
@@ -66,7 +78,7 @@ def main():
     params = module.params
 
     if params['state'] == 'present':
-        install_packages(module, params['name'])
+        install_packages(module, params['name'], params['tool'])
     elif params['state'] == 'absent':
         remove_packages(module, params['name'], params['recurse'])
 
